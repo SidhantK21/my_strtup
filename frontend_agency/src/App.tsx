5f0897f4a9e13b70f837843fd182e1f91c5f07eb
work <work@Akashs-MacBook-Air-2.local>
2025-02-25 16:56:45 +0530

    push code

--- frontend_agency/src/App.tsx
+++ frontend_agency/src/App.tsx
@@ -1,10 +1,25 @@
-
+import  {Navbar}  from './components/Navbar';
+import { Hero } from './components/Herosection';
+import { Offerings } from './components/Offerings';
+import { Contact } from './components/Contact';
+import  {Pricing}  from './components/Pricing';
+import Footer from './components/Footer';
+import PreviousWork from './components/PreviousWork';
+import TechStack from './components/TechStack';
 
 function App() {
   return (
     <div className="bg-black min-h-screen">
+        <Navbar />
+      <Hero />
     
-
+      <PreviousWork />
+      <TechStack />
+      
+      <Offerings />
+      <Contact />
+      <Pricing />
+      <Footer />
     </div>
   );
 }
